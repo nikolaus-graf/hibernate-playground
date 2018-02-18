5c38aa9fc534c9b48b2fb4c98c3fe38c30734b42
nikolaus-graf <nikolaus.a.graf@gmail.com>
2018-02-18 13:27:31 +0100

    initial import

---
+++ src/main/java/org/graf/hibernateplayground/HibernatePlaygroundApplication.java
@@ -0,0 +1,12 @@
+package org.graf.hibernateplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatePlaygroundApplication.class, args);
+	}
+}
