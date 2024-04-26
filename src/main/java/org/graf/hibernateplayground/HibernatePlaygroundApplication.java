c93138bac3b30e6b7739bb5c5e0f4d5757ab63d3
nikolaus-graf <nikolaus.a.graf@gmail.com>
2024-04-26 09:43:29 +0200

    spring version update

--- src/main/java/org/graf/hibernateplayground/HibernatePlaygroundApplication.java
+++ src/main/java/org/graf/hibernateplayground/HibernatePlaygroundApplication.java
@@ -1,16 +1,12 @@
 package org.graf.hibernateplayground;
 
-import org.graf.hibernateplayground.service.PersonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HibernatePlaygroundApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(HibernatePlaygroundApplication.class, args);
-		PersonService personService = applicationContext.getBean(PersonService.class);
-		personService.removeAddressFromPerson();
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(HibernatePlaygroundApplication.class, args);
+  }
 }
